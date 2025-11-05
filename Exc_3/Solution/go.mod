b5abe5c3476608ae577fae57221c1cfbb42363eb
afzaalyasin1 <afzaalyasin@hotmail.com>
2025-11-05 16:44:36 +0100

    Docker Compose for Exercise 4

--- Exc_3/Solution/go.mod
+++ Exc_3/Solution/go.mod
@@ -1,6 +1,6 @@
 module ordersystem
 
-go 1.22
+go 1.23
 
 require (
 	github.com/go-chi/chi/v5 v5.2.3
--- Exc_3/Solution/run.sh
+++ Exc_3/Solution/run.sh
@@ -1,6 +1,26 @@
 #!/bin/sh
 
-# todo
-# docker build
-# docker run db
-# docker run orderservice
+# 1. Stop and remove any old containers
+docker stop pg-db orderservice_app > /dev/null 2>&1
+docker rm pg-db orderservice_app > /dev/null 2>&1
+
+# 2. Run the database
+docker run --name pg-db \
+  -e POSTGRES_DB=order \
+  -e POSTGRES_USER=docker \
+  -e POSTGRES_PASSWORD=docker \
+  -p 5432:5432 \
+  -v pg_data:/var/lib/postgresql/data \
+  --rm -d postgres:16
+
+# Wait a few seconds for the DB to be ready
+echo "Waiting 5s for DB to start..."
+sleep 5
+
+docker build -t orderservice .
+
+
+docker run --name orderservice_app \
+  --network host \
+  --env-file .env \
+  --rm -d orderservice
